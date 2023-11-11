58784cbcd6661931cd4864d5337bd654de538f55
Lee-Si-Yoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2023-11-12 03:18:54 +0900

    install chromatic (#8)

---
+++ src/components/Layouts/Spacing.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SpacingComponent from './Spacing';
+
+const meta: Meta<typeof SpacingComponent> = {
+  component: SpacingComponent,
+};
+
+export default meta;
+type Story = StoryObj<typeof SpacingComponent>;
+
+export const Spacing: Story = {
+  args: {
+    size: 50,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <>
+          <i>vertcal spacing</i>
+          <div style={{ backgroundColor: 'black', height: '50px' }} />
+          <Story />
+          <div style={{ backgroundColor: 'black', height: '50px' }} />
+        </>
+      );
+    },
+  ],
+};
